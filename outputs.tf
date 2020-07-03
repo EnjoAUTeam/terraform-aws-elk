b069f22927d9d0e3a439f08d4120eccb6018e6c7
Lucas van Staden <sales@proxiblue.com.au>
2020-07-03 15:17:18 +0800

    output public ip

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "kibana_url" {
   value       = "http://${aws_eip.ip.public_ip}:5601"
   description = "URL to your ELK server's Kibana web page"
 }
+output "public_ip" {
+  value       = aws_eip.ip.public_ip
+  description = "URL to your ELK server's Kibana web page"
+}
