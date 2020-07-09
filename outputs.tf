f64ad252e66b7c3b0fa930174f11d093d3c98ee3
lucas.vanstaden <lucas.vanstaden@enjo.com.au>
2020-07-09 06:05:15 +0000

    add dns to outputs

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,17 @@ output "kibana_url" {
 }
 output "public_ip" {
   value       = aws_eip.ip.public_ip
-  description = "URL to your ELK server's Kibana web page"
+  description = "Public IP. Can chancge. Pefer to use DNS"
+}
+output "public_dns" {
+  value       = aws_instance.elk.public_dns
+  description = "PublicDNS, should be used as CNAME"
+}
+output "private_dns" {
+  value       = aws_instance.elk.private_dns
+  description = "private DNS. Should be used in CNAME"
+}
+output "private_ip" {
+  value       = aws_instance.elk.private_ip
+  description = "Subject to change. Prefer to use DNS."
 }
